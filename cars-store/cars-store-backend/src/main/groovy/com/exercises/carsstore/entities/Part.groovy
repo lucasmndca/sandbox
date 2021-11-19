16c06ab549cc0fb89aa14b495d3f5331ce20a27b
Lucas Mendonça Dias <lucas.mendonca159@gmail.com>
2021-11-19 03:56:17 -0300

    Completely removes the folder cars-list

--- cars-store/cars-store-backend/src/main/groovy/com/exercises/carsstore/entities/Part.groovy
+++
@@ -1,8 +0,0 @@
-package com.exercises.carsstore.entities
-
-import com.exercises.carsstore.entities.common.BaseEntity
-import org.springframework.stereotype.Component
-
-@Component
-class Part extends BaseEntity{
-}
